e441cea78b3bd8451b6b07ceaa7fc94a988e7704
adam <devmetal91@gmail.com>
2018-02-01 16:12:58 +0100

    Boilerplates for schema defs

---
+++ src/schema/schema.js
@@ -0,0 +1,46 @@
+require('./mongo');
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const Board = require('./board/board.resolver');
+const Comment = require('./comment/comment.resolver');
+const HistoryRecord = require('./history-record/history-record.resolver');
+const Mutation = require('./mutation/mutation.resolver');
+const Query = require('./query/query.resolver');
+const Ticket = require('./ticket/ticket.resolver');
+
+const gqlFile = join.bind(null, __dirname);
+const readFile = file => readFileSync(file, { encoding: 'utf8' });
+
+/**
+ * Schema .gql files
+ */
+const files = [
+  'schema.gql',
+  'board/board.gql',
+  'comment/comment.gql',
+  'history-record/history-item.gql',
+  'history-record/history-record.gql',
+  'mutation/mutation.gql',
+  'query/query.gql',
+  'ticket/ticket-input.gql',
+  'ticket/ticket.gql',
+];
+
+const typeDefs = files
+  .map(gqlFile)
+  .map(readFile)
+  .reduce((acc, curr) => `${acc}${curr}`, '');
+
+const resolvers = {
+  Query,
+  Mutation,
+  Ticket,
+  Board,
+  Comment,
+  HistoryRecord,
+};
+
+module.exports = {
+  typeDefs,
+  resolvers,
+};
