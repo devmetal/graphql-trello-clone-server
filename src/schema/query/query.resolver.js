e713dfe9eabcbad160b8921931d92e1ad373d685
Adam Metal <adam.metal@lensa.com>
2018-10-09 11:35:15 +0200

    :construction: re organize user module. Unit tests not included jet

--- src/schema/query/query.resolver.js
+++ src/schema/query/query.resolver.js
@@ -1,13 +1,1 @@
-const mongoose = require('mongoose');
-
-const User = mongoose.model('User');
-
-module.exports = {
-  currentUser(parent, args, ctx) {
-    if (!ctx.user) return null;
-    return ctx.user;
-  },
-  users() {
-    return User.find({}).sort({ _id: 1 });
-  },
-};
+module.exports = {};
--- src/schema/user/user.resolver.js
+++ src/schema/user/user.resolver.js
@@ -1,10 +1,33 @@
 const axios = require('axios');
+const mongoose = require('mongoose');
+const { createToken } = require('../../auth');
 const config = require('../../config');
 
 const base = config.get('teamServer');
 
+const User = mongoose.model('User');
+
 module.exports = {
   team({ teamId }) {
     return axios.get(`${base}/teams/${teamId}`).then(resp => resp.data);
+  },
+  Query: {
+    currentUser(parent, args, ctx) {
+      if (!ctx.user) return null;
+      return ctx.user;
+    },
+    users() {
+      return User.find({}).sort({ _id: 1 });
+    },
+  },
+  Mutation: {
+    async createUser(parent, { email, password }) {
+      const hased = await User.hash(password);
+      return User.create({ email, password: hased });
+    },
+
+    login(parent, { email, password }) {
+      return createToken({ email, password });
+    },
   },
 };
